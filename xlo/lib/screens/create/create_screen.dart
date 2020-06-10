1956c309db71641beea1c85b422caeae64cd690a
Dieinimy <dieinimy@gmail.com>
2020-06-10 19:42:32 -0300

    292 - 27 - Adicionando o titulo, Descrição e Preço

--- xlo/lib/screens/create/create_screen.dart
+++ xlo/lib/screens/create/create_screen.dart
@@ -1,4 +1,6 @@
+import 'package:brasil_fields/formatter/real_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:xlo/common/custom_drawer/custom_drawer.dart';
 
 import 'widgets/image_field.dart';
@@ -24,10 +26,68 @@ class _CreateScreenState extends State<CreateScreen> {
         child: ListView(
           children: <Widget>[
             ImagesField(
-              onSaved: (images){
-
-              },
+              onSaved: (images) {},
               initialValue: [],
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Título *',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+              ),
+              validator: (text) {
+                if (text.isEmpty) return 'Campo obrigatório';
+                return null;
+              },
+              onSaved: (t) {},
+            ),
+            TextFormField(
+              maxLines: null,
+              decoration: InputDecoration(
+                labelText: 'Descrição *',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+              ),
+              validator: (text) {
+                if (text.trim().isEmpty) return 'Campo obrigatório';
+                if (text.trim().length < 10) return 'Descrição muito curta';
+                return null;
+              },
+              onSaved: (d) {},
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Preço *',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+                signed: false,
+              ),
+              inputFormatters: [
+                WhitelistingTextInputFormatter.digitsOnly,
+                RealInputFormatter(centavos: true),
+              ],
+              validator: (text) {
+                if (text.isEmpty) return 'Campo obrigatório';
+                if (double.tryParse(text) == null)
+                  return 'Utilize valores válidos';
+                return null;
+              },
+              onSaved: (price) {},
             ),
             Container(
               height: 50,
@@ -40,11 +100,8 @@ class _CreateScreenState extends State<CreateScreen> {
                       fontSize: 18,
                       fontWeight: FontWeight.w500),
                 ),
-                onPressed: (){
-                  if(_formKey.currentState.validate()){
-
-                  }
-
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {}
                 },
               ),
             )
