249cee3e998d7e30be105248dbee53a9da5c36c2
Dieinimy <dieinimy@gmail.com>
2020-06-13 16:55:44 -0300

    301 - 36 - Adicionando os anuncios na tela principal

--- xlo/lib/blocs/home_bloc.dart
+++ xlo/lib/blocs/home_bloc.dart
@@ -1,17 +1,24 @@
 import 'package:rxdart/rxdart.dart';
+import 'package:xlo/models/ad.dart';
 
 class HomeBloc {
-
   final BehaviorSubject<String> _searchController = BehaviorSubject<String>();
+  final BehaviorSubject<List<Ad>> _adController = BehaviorSubject<List<Ad>>.seeded([]);
 
   Stream<String> get outSearch => _searchController.stream;
 
-  void setSearch(String search){
+  Stream<List<Ad>> get outAd => _adController.stream;
+
+  void addAd(Ad ad) {
+    _adController.add(_adController.value..add(ad));
+  }
+
+  void setSearch(String search) {
     _searchController.add(search);
   }
 
-  void dispose(){
+  void dispose() {
     _searchController.close();
+    _adController.close();
   }
-
 }
