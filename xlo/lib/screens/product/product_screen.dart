4c1fb7238622fca8770e6100ed2b544d15628126
Dieinimy <dieinimy@gmail.com>
2020-06-13 18:48:15 -0300

    307 - 42 - Botões de Ligar e chat

--- xlo/lib/screens/product/product_screen.dart
+++ xlo/lib/screens/product/product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:xlo/models/ad.dart';
+import 'package:xlo/screens/product/widget/bottom_bar.dart';
 import 'package:xlo/screens/product/widget/location_panel.dart';
 
 import 'widget/description_panel.dart';
@@ -19,37 +20,42 @@ class ProductScreen extends StatelessWidget {
         title: const Text('Anuncio'),
         elevation: 0,
       ),
-      body: ListView(
+      body: Stack(
         children: <Widget>[
-          Container(
-            height: 280,
-            child: Carousel(
-              images: ad.images.map((f) {
-                return FileImage(f);
-              }).toList(),
-              dotSize: 4,
-              dotSpacing: 15,
-              dotBgColor: Colors.transparent,
-              dotColor: Colors.pink,
-              autoplay: false,
-            ),
+          ListView(
+            children: <Widget>[
+              Container(
+                height: 280,
+                child: Carousel(
+                  images: ad.images.map((f) {
+                    return FileImage(f);
+                  }).toList(),
+                  dotSize: 4,
+                  dotSpacing: 15,
+                  dotBgColor: Colors.transparent,
+                  dotColor: Colors.pink,
+                  autoplay: false,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    MainPanel(ad),
+                    Divider(),
+                    DescriptionPanel(ad),
+                    Divider(),
+                    LocationPanel(ad),
+                    Divider(),
+                    UserPanel(ad),
+                    const SizedBox(height: 32,)
+                  ],
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                MainPanel(ad),
-                Divider(),
-                DescriptionPanel(ad),
-                Divider(),
-                LocationPanel(ad),
-                Divider(),
-                UserPanel(ad),
-                const SizedBox(height: 32,)
-              ],
-            ),
-          ),
+          BottomBar(ad),
         ],
       ),
     );
---
+++ xlo/lib/screens/product/widget/bottom_bar.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:xlo/models/ad.dart';
+
+class BottomBar extends StatelessWidget {
+  final Ad ad;
+
+  BottomBar(this.ad);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 38,
+            margin: const EdgeInsets.symmetric(horizontal: 26),
+            decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: const BorderRadius.all(Radius.circular(19))),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 25,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          border:
+                              Border(right: BorderSide(color: Colors.black45))),
+                      child: Text(
+                        'Ligar',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 15),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Chat',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(249, 249, 249, 1),
+                border: Border(top: BorderSide(color: Colors.grey[400]))),
+            height: 38,
+            alignment: Alignment.center,
+            child: Text(
+              'Dieinimy (Anunciante)',
+              style: TextStyle(fontWeight: FontWeight.w300),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
