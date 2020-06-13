1321fd63b9411bf1028c5191f49b4ba8ff85d233
Dieinimy <dieinimy@gmail.com>
2020-06-13 18:57:35 -0300

    308 - 43 - Criando a tela Minha conta

---
+++ xlo/lib/screens/account/account_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:xlo/common/custom_drawer/custom_drawer.dart';
+
+class AccountScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MIniha conta'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('EDITAR'),
+            textColor: Colors.white,
+            onPressed: (){},
+
+          )
+        ],
+        elevation: 0,
+      ),
+      drawer: CustomDrawer(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 170,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            alignment: Alignment.center,
+            child: const Text(
+              'Dieinimy',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Meus anuncios',
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+            ),
+            onTap: () {},
+            trailing: Icon(Icons.keyboard_arrow_right),
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          ListTile(
+            title: Text(
+              'Favoritos',
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+            ),
+            onTap: () {},
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            trailing: Icon(Icons.keyboard_arrow_right),
+          ),
+        ],
+      ),
+    );
+  }
+}
